41be68f3f481fe75081293257e3930b7fd6a6b29
Jeff Dinkelman <mfankenstein@yahoo.com>
2019-09-24 13:45:49 -0700

    config folder; scratchpad

---
+++ scratchpad.js
@@ -0,0 +1,40 @@
+// const http = require('http');
+//
+// const data = {
+//   "name": "jeff",
+//   "diet": "vegan"
+// };
+//
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     res.write('Hello to me');
+//     res.end();
+//   };
+//
+//   if (req.url === '/api/data') {
+//     res.write(JSON.stringify(data));
+//     res.end();
+//   };
+// });
+
+// const server = http.createServer((req, res) => {
+//   res.setHeader({"Content-Type": "application/json"});
+//   res.writeHead(200);
+//   res.end();
+// });
+
+// server.listen(5000, () => {
+//   console.log('Server is listening on port 5000');
+// });
+
+const countLetter = (word, character) => {
+  let count = 0;
+  for (let letter of word) {
+    if(letter === character) {
+      count++;
+    };
+  };
+  return count;
+};
+
+console.log(countLetter('banana', 'a'));
