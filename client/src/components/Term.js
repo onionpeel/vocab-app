f75d80baa6837112c5699adb03aa0ef1a7313685
Jeff Dinkelman <mfankenstein@yahoo.com>
2019-10-31 09:41:49 -0700

    Updates style of Term.js and VocabListTerm.js to re-set location of buttons

--- client/src/components/Term.js
+++ client/src/components/Term.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, Row, Col, Button} from 'react-bootstrap';
+import {Card, Row, Col, Button, Container} from 'react-bootstrap';
 import {connect} from 'react-redux';
 import {add} from '../actions/vocabActions';
 import uuid from 'uuid/v4';
@@ -17,30 +17,35 @@ const Term = ({kanji, kana, english, add}) => {
   return (
     <Card style={{marginBottom: 10, padding: 10, boxShadow: "1px 2px 5px grey",
                   backgroundColor: 'rgb(245, 250, 250)'}}>
-      <Card.Body className="align-self-end">
-        <Button onClick={handleOnClick}>Add</Button>
-      </Card.Body>
-      <Row>
-        <Col xs={1}></Col>
-        <Col xs={10}>
-          <Row>
-            <h2>{kanji}</h2>
-          </Row>
-          <Row>
-          <p style={{fontSize:"125%"}}>{kana} </p>
-          </Row>
-          <Row>
-            <ol>
-              {english.map(def => (
-                <li key={uuid()}>
-                  <p style={{fontFamily: 'Helvetica', fontSize:"125%"}}>{def.english_definitions.join('; ')}</p>
-                </li>
-              ))}
-            </ol>
-          </Row>
-        </Col>
-        <Col xs={1}></Col>
-      </Row>
+
+      <Container>
+        <Row style={{display: 'flex', justifyContent: 'flex-end'}}>
+          <Button onClick={handleOnClick}>Add</Button>
+        </Row>
+
+        <Row>
+          <Col xs={1}></Col>
+          <Col xs={10}>
+            <Row>
+              <h2>{kanji}</h2>
+            </Row>
+            <Row>
+            <p style={{fontSize:"125%"}}>{kana} </p>
+            </Row>
+            <Row>
+              <ol>
+                {english.map(def => (
+                  <li key={uuid()}>
+                    <p style={{fontFamily: 'Helvetica', fontSize:"125%"}}>{def.english_definitions.join('; ')}</p>
+                  </li>
+                ))}
+              </ol>
+            </Row>
+          </Col>
+          <Col xs={1}></Col>
+        </Row>
+      </Container>
+
     </Card>
   );
 };
--- scratchpad.js
+++ scratchpad.js
@@ -1,5 +1,62 @@
+
+
+
 <div>{String.fromCharCode(0x2713)} Added</div>
 <div>{"\u2713 Added"}</div>
+
+  import React from 'react';
+  import {Card, Row, Col, Button} from 'react-bootstrap';
+  import {connect} from 'react-redux';
+  import {add} from '../actions/vocabActions';
+  import uuid from 'uuid/v4';
+
+  const Term = ({kanji, kana, english, add}) => {
+    const handleOnClick = e => {
+      e.preventDefault();
+      add({
+        kanji,
+        kana,
+        english
+      });
+    };
+
+    return (
+      <Card style={{marginBottom: 10, padding: 10, boxShadow: "1px 2px 5px grey",
+                    backgroundColor: 'rgb(245, 250, 250)'}}>
+        <Card.Body className="align-self-end">
+          <Button onClick={handleOnClick}>Add</Button>
+        </Card.Body>
+        <Row>
+          <Col xs={1}></Col>
+          <Col xs={10}>
+            <Row>
+              <h2>{kanji}</h2>
+            </Row>
+            <Row>
+            <p style={{fontSize:"125%"}}>{kana} </p>
+            </Row>
+            <Row>
+              <ol>
+                {english.map(def => (
+                  <li key={uuid()}>
+                    <p style={{fontFamily: 'Helvetica', fontSize:"125%"}}>{def.english_definitions.join('; ')}</p>
+                  </li>
+                ))}
+              </ol>
+            </Row>
+          </Col>
+          <Col xs={1}></Col>
+        </Row>
+      </Card>
+    );
+  };
+
+  const mapDispatchToState = {
+    add
+  };
+
+  export default connect(null, mapDispatchToState)(Term);
+
 
 // const english = [{english_definitions: ['a', 'b', 'c']}];
 // console.log(english[0].english_definitions.join('; '));
