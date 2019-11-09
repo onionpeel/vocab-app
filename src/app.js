e89bca7360b7120d2b0ae2fede2a9664d8c72445
Jeff Dinkelman <mfankenstein@yahoo.com>
2019-11-09 13:28:50 -0800

    adds cors back to app.js

--- src/app.js
+++ src/app.js
@@ -8,11 +8,11 @@ app.use(bodyParser.json());
 
 dbConnect();
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/api/user', require('./routes/api/user'));
 app.use('/api/vocab', require('./routes/api/vocab'));
