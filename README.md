7377835cc966b262a025b379c97d8abacf1e8a1c
Jeff Dinkelman <mfankenstein@yahoo.com>
2019-11-21 19:56:40 -0800

    adds a list of project features to About.js and README.md

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
-#Vocab-app
+# Vocab-app
 
-This application allows a user to search for words in a Japanese dictionary.  The user can create an account and then the results from the word search can be added to create a personalized vocabulary list.
+This application allows a user to search for words in a Japanese dictionary.  The user can create an account and create a personalized vocabulary list.
 
-##Installation
+# Features
+-The frontend is built with React, Redux, Bootstrap and React-Bootstrap
+-React-Router handles the client-side routing
+-User input is stored in a MongoDB database, using Mongoose to model the data
+-Dictionary searches are cached using Redis
+-The backend runs on an Express server
+-Express-Validator validates form input
+-bcryptjs encrypts passwords
+-JSON web tokens are used to maintain a user's activity in local storage
+-Asynchronous calls are made using Axios
+-Project configurations are organized through the npm package, config
+
+## Installation
 ```
 git clone git@github.com:onionpeel/vocab-app.git
 cd vocab-app
@@ -10,10 +22,10 @@ npm install
 npm client-install
 
 ```
-Redis is used for caching the results of the dictionary searches, so when running locally, a Redis instance needs to be created.
+Redis is used for caching the results of the dictionary searches. A Redis instance needs to be created for the searches to work.
 MongoDB is the backend database.  This can be set up through the MongoDB website.
 
-###Configuration
+### Configuration
 Local
 In the config directory create a `default.json` file.  Set up that file as follows, filling in the necessary information
 
@@ -26,6 +38,6 @@ In the config directory create a `default.json` file.  Set up that file as follo
 Production
 JWT_PRIVATE_KEY, MONGODB_URI and REDIS_URL should be set directly on the server.  These will override any values set in `config/default.json`.
 
-###Run
+### Run
 `npm run dev`
 This starts one server for the front end and another for the backend application.
--- client/src/components/About.js
+++ client/src/components/About.js
@@ -12,7 +12,7 @@ const About = () => {
             <h3 className="image-title">The Powdered Donut Sisters</h3>
           </Col>
         </Row>
-        <Row style={{marginTop: 10}}>
+        <Row style={{marginTop: 10, marginBottom: 10}}>
           <Col xs={12} md={8}>
             <Image src="assets/donutCropped.jpg" className="about-profile-pic img-fluid" rounded />
             <p>Don't mistake us for the Q-tip siblings</p>
@@ -20,8 +20,27 @@ const About = () => {
             <p>The power of your inner pastry is waiting to be unleashed.</p>
           </Col>
           <Col>
-            <p>Remember that no makeover is complete without visiting the <a style={{color: '#2892D7'}} href="https://github.com/onionpeel/vocab-app">source code</a>.</p>
-            <p>This is a whimsical site demonstrating serious full stack tech.</p>
+            <p><b>Okay, the truth is that this website has nothing to do with the powdered donut sisters (even though they are a wonderful trio).</b></p>
+            <p><b>Actually, this is a whimsical site demonstrating serious full stack tech.</b></p>
+            <p><b>Remember that no makeover is complete without visiting the <a style={{color: '#2892D7'}} href="https://github.com/onionpeel/vocab-app">source code</a>.</b></p>
+        </Col>
+        </Row>
+        <Row>
+          <Col xs={12} md={7} style={{marginBottom: 10, padding: 10, boxShadow: "1px 2px 5px grey",
+                        backgroundColor: 'rgb(245, 250, 250)'}} className="features">
+            <h3>Features of the Sumo Diaper Rash</h3>
+            <ul>
+              <li>The frontend is built with React, Redux, Bootstrap and React-Bootstrap</li>
+              <li>React-Router handles the client-side routing</li>
+              <li>User input is stored in a MongoDB database, using Mongoose to model the data</li>
+              <li>Dictionary searches are cached using Redis</li>
+              <li>The backend runs on an Express server</li>
+              <li>Express-Validator validates form input</li>
+              <li>bcryptjs encrypts passwords</li>
+              <li>JSON web tokens are used to maintain a user's activity in local storage</li>
+              <li>Asynchronous calls are made using Axios</li>
+              <li>Project configurations are organized through the npm package, config</li>
+            </ul>
           </Col>
         </Row>
       </Container>
